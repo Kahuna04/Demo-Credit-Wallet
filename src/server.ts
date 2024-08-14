5234b3cc84699b4879770142d6408f11b5e6d158
Damilare O <ogundele.damilare4@gmail.com>
2024-08-14 19:16:44 +0100

    fix: transaction routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoutes from "./routes/userRoute";
+import transactionRoutes from "./routes/transactionRoute"
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -8,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api', userRoutes);
+app.use('/api', userRoutes, transactionRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
